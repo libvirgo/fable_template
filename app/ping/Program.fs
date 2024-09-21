0dfc0c577a70618716f6ae5b684f73b18ffdf527
libvirgo <sakurapetgirl@live.com>
2024-09-22 01:17:39 +0800

    chore(app/ping): add overload type example.

--- app/ping/Program.fs
+++ app/ping/Program.fs
@@ -9,13 +9,17 @@ type Response =
 
 [<Erase>]
 type Fastify =
-    abstract get<'T> : string -> (obj -> Response -> JS.Promise<'T>) -> unit
+    abstract get<'T> : path: string * handler: (obj -> Response -> JS.Promise<'T>) -> unit
+    abstract get<'T> : path: string * handler: (unit -> JS.Promise<'T>) -> unit
     abstract listen: {| port: int |} -> unit
 
 [<ImportDefault("fastify")>]
 let fastify: {| logger: bool |} -> Fastify = jsNative
 
+let lambda x = fun () -> x
 let app = fastify {| logger = true |}
 
-app.get "/" (fun _ _ -> (promise { return {| ping = "pong" |} }))
+app.get ("/ping", (fun _ _ -> promise { return {| ping = "pong" |} }))
+app.get ("/hello", lambda (promise { return "hello world" }))
+
 app.listen {| port = 3000 |}
