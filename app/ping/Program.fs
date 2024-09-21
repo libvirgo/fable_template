23539d75a8feb035999d56cf5d7cb1309488fdce
libvirgo <sakurapetgirl@live.com>
2024-09-21 23:03:05 +0800

    init(fable): fable template project.

---
+++ app/ping/Program.fs
@@ -0,0 +1,21 @@
+﻿module Template.Ping
+
+open Fable.Core
+
+[<Erase>]
+type Response =
+    inherit JS.Object
+    abstract send: obj -> unit
+
+[<Erase>]
+type Fastify =
+    abstract get<'T> : string -> (obj -> Response -> JS.Promise<'T>) -> unit
+    abstract listen: {| port: int |} -> unit
+
+[<ImportDefault("fastify")>]
+let fastify: {| logger: bool |} -> Fastify = jsNative
+
+let app = fastify {| logger = true |}
+
+app.get "/" (fun _ _ -> (promise { return {| ping = "pong" |} }))
+app.listen {| port = 3000 |}
